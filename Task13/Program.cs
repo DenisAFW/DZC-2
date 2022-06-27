f4aaa84595333fda47bab6c8e88b398c81b76649
Choppa <denis.afinogenow@yandex.ru>
2022-06-27 18:29:04 +0300

    оставил неправильный вариант, и сделал верный

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число не более 1000");
+﻿/*
+// Неверное решение
+Console.WriteLine("Введите число не более 1000");
 int a = Convert.ToInt32(Console.ReadLine());
 void ThirdNumber(int a)
 {
@@ -25,3 +27,28 @@ void ThirdNumber(int a)
     }
 }
 ThirdNumber(a);
+*/
+
+Console.WriteLine("Введите число.");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int ThirdNumber()
+{
+    if(a<0)
+    {
+        a = a * -1;
+    }
+    if(a<100)
+    {
+        Console.WriteLine("Третье число отсутствует");
+    }
+    if(a>=100)
+    {
+        while(a>1000)
+        a = a/10;
+    }
+    int Third = a % 10;
+    Console.WriteLine("Третье число - " + Third);
+    return Third;
+}
+ThirdNumber();
